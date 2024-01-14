82a98da6bd1c22eac844055ac2a3a1cbecf3c21f
Melissa <77360593+mscedeno@users.noreply.github.com>
2024-01-14 15:04:47 -0500

    Update main.cpp
    ajuste rango del sensor de gas

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include <FirebaseESP32.h>
 #include <addons/RTDBHelper.h>
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "WIFI_AP"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
+#define WIFI_SSID "SOLANGE-YIGA5-2.4G"
+#define WIFI_PASSWORD "Solang1720"
 /* 2. Define the RTDB URL */
 #define DATABASE_URL "fir-se-ceabc-default-rtdb.firebaseio.com"
 
@@ -14,23 +14,25 @@
 int gas = 36;
 Servo servo_1;
 int boton=15;
+
 int pinVerde = 27;
 int pinAmarillo = 25;
 int pinRojo = 33;
+
 int sonido = 21;
 int lastState = HIGH;
+int lastState2 = HIGH;
+
 int pinServo = 26;
 int Signal = 0;
-int motor1 = 18;
-int motor12 = 19;
-int motor2 = 17;
-int motor22 = 16;
+
+int motor1 = 19;
+int motor2 = 16;
+
 // Setting PWM properties
 const int freq = 30000;
 const int canalPWM1 = 0;
 const int canalPWM2 = 1;
-const int canalPWM3 = 0;
-const int canalPWM4 = 1;
 const int resolution = 8;
 
 //variables modo manual
@@ -42,7 +44,13 @@ FirebaseAuth auth;
 FirebaseConfig config;
 int valor = 0;
 int porcentual = 0;
-String modo = "";
+String porcen = "";
+
+String Nmodo = "0";
+String Npuerta = "0";
+String Nextractor = "0";
+String Nventilador = "0";
+
 
 void setup()
 {
@@ -57,11 +65,10 @@ void setup()
   // Configurar la señal PWM
   ledcSetup(canalPWM1, freq, resolution);
   ledcSetup(canalPWM2, freq, resolution);
+
   // Asignar el pin al canal PWM
   ledcAttachPin(motor1, canalPWM1);
-  ledcAttachPin(motor12, canalPWM2);
-  ledcAttachPin(motor2, canalPWM3);
-  ledcAttachPin(motor22, canalPWM4);
+  ledcAttachPin(motor2, canalPWM2);
 
   servo_1.attach(pinServo, 500, 2500); 
   servo_1.write(0);
@@ -91,24 +98,37 @@ void setup()
 
 void loop()
 {
-    //Datos importantes
+  //Datos importantes
   Signal = analogRead(gas);
-  //Mostramos la señal del sensor de gas
   Serial.println(Signal); //muestra el valor del sensor de gas
-  delay(1000);
+  delay(600);
 
-  int pwm_value = map(Signal, 1700, 2200, 90, 190);
+  int pwm_value = map(Signal, 200, 900, 90, 190);
 
-  valor = map(Signal, 1700, 2200, 0, 100);
+  valor = map(Signal, 0, 900, 0, 100);
   porcentual = map(valor, 0, 100, 100, 0);
-  char porcen = porcentual;
+  porcen = porcentual;
   Firebase.setString(fbdo,"/ProyectoSEV2/porcentaje", porcen);
 
   //ESTADOS
+  Firebase.getString(fbdo,"/ProyectoSEV2/Npuerta");
+  Npuerta = fbdo.stringData();
+  
+  Firebase.getString(fbdo,"/ProyectoSEV2/Nextractor");
+  Nextractor = fbdo.stringData();
 
+  Firebase.getString(fbdo,"/ProyectoSEV2/Nventilador");
+  Nventilador = fbdo.stringData();
 
-// Modo automatico
-    if(Signal < 1720) //calidad del aire buena
+  //MODOS
+  Firebase.getString(fbdo,"/ProyectoSEV2/Nmodo");
+  Nmodo = fbdo.stringData();
+  
+  
+  // Modo automatico
+
+  if (Nmodo.equals("0")){
+    if(Signal < 300) //calidad del aire buena
     {
         digitalWrite(pinVerde, HIGH);
         digitalWrite(pinAmarillo, LOW);
@@ -123,34 +143,39 @@ void loop()
         Firebase.setString(fbdo,"/ProyectoSEV2/estadoventilador","desactivado");   
     }
 
-    if ((Signal > 1720) and (Signal < 1730)) //calidad del aire regular
+    else if ((Signal > 300) and (Signal < 600)) //calidad del aire regular
     {
         digitalWrite(pinVerde, LOW);
         digitalWrite(pinAmarillo, HIGH);
         digitalWrite(pinRojo, LOW);
+        delay(25);
         digitalWrite(sonido,LOW);
         servo_1.write(0);
         ledcWrite(canalPWM1, pwm_value);
         Firebase.setString(fbdo,"/ProyectoSEV2/calidad","Regular");
         Firebase.setString(fbdo,"/ProyectoSEV2/estadopuerta","cerrado");
-        Firebase.setString(fbdo,"/ProyectoSEV2/estadoextractor","desactivado");
-        Firebase.setString(fbdo,"/ProyectoSEV2/estadoventilador","desactivado");
+        Firebase.setString(fbdo,"/ProyectoSEV2/estadoextractor","activado");
+        Firebase.setString(fbdo,"/ProyectoSEV2/estadoventilador","activado");
+        digitalWrite(pinAmarillo, LOW);
+
     }
 
-    if(Signal > 2000) //calidad del aire mala
+    else if(Signal > 600) //calidad del aire mala
     {
         digitalWrite(pinVerde, LOW);
         digitalWrite(pinAmarillo, LOW);
         digitalWrite(pinRojo, HIGH);
+        delay(15);
         servo_1.write(90); //abre la puerta cuando exite mala calidad de mala
         digitalWrite(sonido, lastState);
-        //delay(1000);
+        delay(100);
         ledcWrite(canalPWM1, pwm_value);
-        ledcWrite(canalPWM4, pwm_value);
+        ledcWrite(canalPWM2, pwm_value);
         Firebase.setString(fbdo,"/ProyectoSEV2/calidad","Mala");
         Firebase.setString(fbdo,"/ProyectoSEV2/estadopuerta","abierto");
         Firebase.setString(fbdo,"/ProyectoSEV2/estadoextractor","activado");
         Firebase.setString(fbdo,"/ProyectoSEV2/estadoventilador","activado");
+        digitalWrite(pinRojo, LOW);
     
     }
 
@@ -163,7 +188,75 @@ void loop()
     }
     int currentState = digitalRead(sonido); //estado actual
     lastState = currentState;
+    delay(30);
+  }
   
   //Modo manual
 
+
+  if (Nmodo.equals("1")){
+
+    if(Signal < 300) //calidad del aire buena
+    {
+        digitalWrite(pinVerde, HIGH);
+        servo_1.write(0);
+        Firebase.setString(fbdo,"/ProyectoSEV2/calidad","Buena");
+
+        if(Nventilador.equals("1")){
+            ledcWrite(canalPWM1, 100);
+        }
+        else if(Nextractor.equals("1")){
+            ledcWrite(canalPWM2, 100);
+        }
+        else if(Npuerta.equals("1")){
+            servo_1.write(90);
+        }
+    }
+    else if ((Signal > 300) and (Signal < 600)) //calidad del aire regular
+    {
+        digitalWrite(pinVerde, LOW);
+        digitalWrite(pinAmarillo, HIGH);
+        digitalWrite(pinRojo, LOW);
+        digitalWrite(sonido,LOW);
+        servo_1.write(0);
+        Firebase.setString(fbdo,"/ProyectoSEV2/calidad","Regular");
+
+        if(Nventilador.equals("1")){
+            ledcWrite(canalPWM1, 100);
+        }
+        else if(Nextractor.equals("1")){
+            ledcWrite(canalPWM2, 100);
+        }
+        else if(Npuerta.equals("1")){
+            servo_1.write(90);
+        }
+    }
+    else if(Signal > 600) //calidad del aire mala
+    {
+        digitalWrite(pinVerde, LOW);
+        digitalWrite(pinAmarillo, LOW);
+        digitalWrite(pinRojo, HIGH);
+        digitalWrite(sonido, lastState2);
+        Firebase.setString(fbdo,"/ProyectoSEV2/calidad","Mala");
+
+        if(Nventilador.equals("1")){
+            ledcWrite(canalPWM1, 100);
+        }
+        else if(Nextractor.equals("1")){
+            ledcWrite(canalPWM2, 100);
+        }
+        else if(Npuerta.equals("1")){
+            servo_1.write(90);
+        }
+    }
+    int buttonState = digitalRead(boton); // read new state
+    Serial.println(buttonState);
+    if (buttonState == LOW) {
+        Serial.println("The button is being pressed");
+        lastState2 = LOW;
+        digitalWrite(sonido, lastState2); // turn on
+    }
+    int currentState2 = digitalRead(sonido); //estado actual
+    lastState2 = currentState2;
+  }
 }
