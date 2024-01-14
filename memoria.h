c5aa42d3d97f5eec3b385ec308c843315a8b085e
Melissa <77360593+mscedeno@users.noreply.github.com>
2024-01-14 18:58:58 -0500

    eeprommemoria.h

--- memoria.h
+++ memoria.h
@@ -1,1 +1,27 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+
+//escribir cadena en memoria eeprom
+void escribirStringEnEEPROM(String cadena, int direccion) {
+  int longitudCadena = cadena.length();
+  for (int i = 0; i < longitudCadena; i++) {
+    EEPROM.write(direccion + i, cadena[i]);
+  }
+  EEPROM.write(direccion + longitudCadena, '\0'); // Null-terminated string
+  EEPROM.commit(); // Guardamos los cambios en la memoria EEPROM
+}
+
+//leer cadena en memoria eeprom
+String leerStringDeEEPROM(int direccion) {
+  String cadena = "";
+  char caracter = EEPROM.read(direccion);
+  int i = 0;
+  while (caracter != '\0' && i < 100) {
+    cadena += caracter;
+    i++;
+    caracter = EEPROM.read(direccion + i);
+  }
+  return cadena;
+}
+
 
